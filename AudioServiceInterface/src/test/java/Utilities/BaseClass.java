372ef68fa3d98aca3f5c09ca3aede79a324e32a7
SuneethaAritakula <suneethaaritakula@yahoo.com>
2020-03-15 13:20:36 +0000

    first commit

---
+++ AudioServiceInterface/src/test/java/Utilities/BaseClass.java
@@ -0,0 +1,38 @@
+package Utilities;
+
+
+	
+	import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.WebElement;
+	import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+	public abstract class BaseClass {
+
+	    private static WebDriver driver;
+	    private static WebDriverWait wait;
+
+	    public BaseClass(WebDriver driver, WebDriverWait wait) {
+
+	        BaseClass.driver = driver;
+	        BaseClass.wait = wait;
+	    }
+
+	    protected void WaitUntilElementVisible(WebElement element) {
+
+	        wait.until(ExpectedConditions.visibilityOf(element));
+	    }
+	    
+	    public WebElement findElement(By by) {
+	    	return driver.findElement(by);
+	    }
+	    
+	    public void selectdropDownValue(WebDriver driver, WebElement element, String value) {
+			Select selectfromdropdown = new Select(element);
+			
+		//	dropdown.selectByVisibleText(value);
+	}
+	}
+
